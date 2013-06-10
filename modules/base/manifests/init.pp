e90a20d22e0dbf087957e8d5ae3e9592a7213102
Geronimo Orozco <geronimo.orozco@intel.com>
2013-06-10 13:27:37 -0500

    Remove intel proxy and local repos

--- modules/base/manifests/init.pp
+++ modules/base/manifests/init.pp
@@ -1,23 +1,6 @@
 class base {
-  file {"/etc/gitconfig":
-  	owner => root,
-	group => root,
-	mode => '0644',
-	content => template('base/gitconfig.erb'),
-  } ->
-  file {"/etc/apt/apt.conf":
-  	owner => root,
-	group => root,
-	mode => '0644',
-	content => template('base/apt.conf.erb'),
-  } ~>
-  exec {"add proxy to global exports":
-    command => 'echo "export http_proxy=http://proxy-us.intel.com:911" >> /etc/environment; echo "export https_proxy=http://proxy-us.intel.com:911" >> /etc/environment; echo "no_proxy=127.0.0.1,localhost,10.0.0.0/8,${ipaddress_eth1}" >> /etc/environment',
-    refreshonly => true,
-  } ~>
   exec {"update apt":
     command => "/usr/bin/apt-get -y update;/usr/bin/apt-get -y dist-upgrade",
-    #command => "apt-get -y update",
     refreshonly => true,
     timeout => 0,
     logoutput => true
