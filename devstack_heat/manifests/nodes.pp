2a207a96b7b2faf704514db7b2ce7e6db1b4524a
Geronimo Orozco <geronimo.orozco@intel.com>
2013-09-16 00:44:57 -0500

    No proxy settings

--- devstack_heat/manifests/nodes.pp
+++ devstack_heat/manifests/nodes.pp
@@ -6,25 +6,26 @@ Stage['main'] -> Stage['post']
 # Base
 
 node basenode {
-  class { 'proxy': 
-        http_proxy_host  =>  "proxy-us.intel.com", 
-        http_proxy_port  => "911", 
-        https_proxy_host =>  "proxy-us.intel.com", 
-        https_proxy_port => "911", 
-        socks_proxy_host =>  "proxy-skype.intel.com", 
-        socks_proxy_port => "1080", 
-        no_proxy_domains => ".intel.com",
-        stage => pre;
-    }
-  class { "base2":        stage => pre, require => Class['proxy']; }
-  class { "mongodb":       stage => pre, require => Class['proxy']; }
+    #class { 'proxy': 
+    #    http_proxy_host  =>  "proxy-us.intel.com", 
+    #    http_proxy_port  => "911", 
+    #    https_proxy_host =>  "proxy-us.intel.com", 
+    #    https_proxy_port => "911", 
+    #    socks_proxy_host =>  "proxy-skype.intel.com", 
+    #    socks_proxy_port => "1080", 
+    #    no_proxy_domains => ".intel.com",
+    #    stage => pre;
+    #}
+    class { "base2": stage           => pre; }
+    # class { "base2":        stage  => pre, require => Class['proxy']; }
+    # class { "mongodb":       stage => pre, require => Class['proxy']; }
 }
 
 #--------------------------------------------
 
 node default inherits basenode {
 
-  class { "devstack_heat": devstack_branch => "stable/grizzly"; }
+  class { "devstack_heat": devstack_branch => "master"; }
 
   group { "vagrant": ensure => "present"; } ->
   user  { "vagrant": ensure => "present"; } 
